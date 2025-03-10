9fa47bb5e8b8e268f1fac188a8c47edf50e17224
i CEP <josephchongola43@gmail.com>
2025-03-10 18:56:39 +0300

    modified:   models/message.model.js

--- models/message.model.js
+++ models/message.model.js
@@ -4,7 +4,7 @@ const messageSchema = new mongoose.Schema(
     {
         name: { type: String, required: true },
         email: { type: String, required: true },
-        pnoneNumber: { type: String},
+        phoneNumber: { type: String},
         message: { type: String, required: true },
         status: { type: String, default: "pending" },
     },
